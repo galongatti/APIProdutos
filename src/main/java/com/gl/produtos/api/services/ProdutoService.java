8b8f633ef6cf660771c1546b3fe651e288950b6d
galongatti <galongatti@hotmail.com>
2025-04-09 21:41:48 -0300

    API Produtos parte 1

---
+++ src/main/java/com/gl/produtos/api/services/ProdutoService.java
@@ -0,0 +1,50 @@
+package com.gl.produtos.api.services;
+
+
+import com.gl.produtos.api.models.Produto;
+import com.gl.produtos.api.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public Produto salvarProduto(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Produto buscarProdutoPorId(Integer id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    public List<Produto> buscarTodosProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    public void deletarProduto(Integer id) throws Exception {
+
+        Produto produtoExistente = produtoRepository.findById(id).orElse(null);
+
+        if(produtoExistente == null) {
+            throw new Exception("Produto não encontrado");
+        }
+        produtoRepository.deleteById(id);
+    }
+
+    public Produto atualizarProduto(Produto produto) throws Exception {
+
+        Produto produtoExistente = produtoRepository.findById(produto.getId()).orElse(null);
+
+        if(produtoExistente == null) {
+            throw new Exception("Produto não encontrado");
+        }
+        return produtoRepository.save(produto);
+    }
+
+}
